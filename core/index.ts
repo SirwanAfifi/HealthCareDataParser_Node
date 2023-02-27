6d67d720002a5154cfe7c1659f49274d0b639e07
Sirwan <sir1afifi@gmail.com>
2023-02-27 13:41:01 +0000

    update

--- core/index.ts
+++ core/index.ts
@@ -11,15 +11,15 @@ import { RecordEntity } from "./entities/RecordEntity";
   for (let i = 0; i < res.Record.length; i++) {
     const item = res.Record[i];
     let record = new RecordEntity();
-    record.type = item.Root.type;
-    record.sourceName = item.Root.sourceName;
-    record.sourceVersion = item.Root.sourceVersion;
-    record.device = item.Root.device;
-    record.unit = item.Root.unit;
-    record.value = item.Root.value;
-    record.creationDate = item.Root.creationDate;
-    record.startDate = item.Root.startDate;
-    record.endDate = item.Root.endDate;
+    record.type = item.Root.type || "unknown";
+    record.sourceName = item.Root.sourceName || "unknown";
+    record.sourceVersion = item.Root.sourceVersion || "unknown";
+    record.device = item.Root.device || "unknown";
+    record.unit = item.Root.unit || "unknown";
+    record.value = item.Root.value || "unknown";
+    record.creationDate = item.Root.creationDate || new Date();
+    record.startDate = item.Root.startDate || new Date();
+    record.endDate = item.Root.endDate || new Date();
     await dbConnection.manager.save(record);
   }
 })();
